5c2fc9df195f51e88b16c27a76acaac8d67be9df
Tianhe Wang <tianhewang166@gmail.com>
2026-04-16 10:56:28 +0200

    Add files via upload

---
+++ Distingush.py
@@ -0,0 +1,18 @@
+import csv
+
+def filter_nagr_data(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
+        reader = csv.DictReader(infile)
+        fieldnames = reader.fieldnames
+        
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for row in reader:
+            if row['Aptamer_Type'] == 'NagR':
+                writer.writerow(row)
+
+# 使用示例
+input_file = 'processed_results_combined_by_pear.csv'
+output_file = 'NagR_new_constant.csv'
+filter_nagr_data(input_file, output_file)
